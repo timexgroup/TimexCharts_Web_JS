3bb3678c58e4c343575246e87ac5b2fc1b818ae8
lbellotindos <39514789+lbellotindos@users.noreply.github.com>
2024-01-19 16:05:49 +0800

    Initial commit
    Initial commit

---
+++ timexcharts.js
@@ -0,0 +1,57 @@
+class StepCountBarChart {
+    constructor(ctx, data) {
+        this.chart = new Chart(this.ctx, {
+            type: 'bar',
+            data: data,
+            options: this.chartOptions
+        });
+    }
+
+    update(newData) {
+        this.chart.data = newData;
+        this.chart.update();
+    }
+
+    // Other methods...
+
+    chartOptions = {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    };
+}
+
+class SleepQualityBarChart {
+    constructor(ctx, data) {
+        this.chart = new Chart(this.ctx, {
+            type: 'bar',
+            data: data,
+            options: this.chartOptions
+        });
+    }
+
+    update(newData) {
+        this.chart.data = newData;
+        this.chart.update();
+    }
+
+    // Other methods...
+
+    chartOptions = {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    };
+}
+
+// Export both classes for use in other scripts
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = { StepCountBarChart, SleepQualityBarChart };
+} else {
+    window.StepCountBarChart = StepCountBarChart;
+    window.SleepQualityBarChart = SleepQualityBarChart;
+}
