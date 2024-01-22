a385ebaa3764a31737e2579bf58a9d0602f20c62
lbellotindos <39514789+lbellotindos@users.noreply.github.com>
2024-01-22 13:39:38 +0800

    Updates
    updates

--- timexcharts.js
+++ timexcharts.js
@@ -1,10 +1,63 @@
-// Import and re-export classes and constants
-const StepCountBarChart = require('./stepcountbarchart.js');
-const SleepQualityBarChart = require('./sleepqualitybarchart.js');
-const { TimeDisplayFormat } = require('./timedisplayformat.js');
+class StepCountBarChart {
+    constructor(ctx, data) {
+        this.ctx = ctx;
 
-module.exports = {
-  StepCountBarChart,
-  SleepQualityBarChart,
-  TimeDisplayFormat,
-};
+        console.log("Hello");
+        this.chart = new Chart(this.ctx, {
+            type: 'bar',
+            data: data,
+            options: this.chartOptions
+        });
+    }
+
+    update(newData) {
+        this.chart.data = newData;
+        this.chart.update();
+    }
+
+    // Other methods...
+
+    chartOptions = {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    };
+}
+
+class SleepQualityBarChart {
+    constructor(ctx, data) {
+        this.ctx = ctx;
+
+        console.log("Hello");
+        this.chart = new Chart(this.ctx, {
+            type: 'bar',
+            data: data,
+            options: this.chartOptions
+        });
+    }
+
+    update(newData) {
+        this.chart.data = newData;
+        this.chart.update();
+    }
+
+    // Other methods...
+
+    chartOptions = {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    };
+}
+
+// Export both classes for use in other scripts
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = { StepCountBarChart, SleepQualityBarChart };
+} else {
+    window.StepCountBarChart = StepCountBarChart;
+    window.SleepQualityBarChart = SleepQualityBarChart;
+}
